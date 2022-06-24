9e6d87ec2d9d3b72526617bf173ee357570073cb
lens147 <bartunior@gmail.com>
2022-06-24 13:28:18 +0200

    rest

--- vue/single.php
+++ vue/single.php
@@ -5,15 +5,15 @@
 
         $titre = "Mon Article";
 
-        $article_id = $_GET['id'];
+        $id_oeuvre = $_GET['id'];
 
         include('../services/crud.php');
 
         $db = new Crud("mysql:host=localhost;dbname=projetfinal;charset=utf8mb4", "root", "");
 
-        $articles = $db->oneOeuvre($article_id);
+        $oeuvres = $db->oneOeuvre($id_oeuvre);
 
-        $id_user = $articles[0]['user_key'];
+        $id_user = $oeuvres[0]['user_key'];
 
         include('./layout/head.php');
 
@@ -31,11 +31,11 @@
             <div class="col blog">
                 <div class="card h-100">
                     <div class="card-body">
-                        <h2 class="card-title text-center text-capitalize"><?php echo $articles[0]['title']; ?></h2>
+                        <h2 class="card-title text-center text-capitalize"><?php echo $oeuvres[0]['titre']; ?></h2>
                         <p class="card-text"></p>
-                        <p class="text-muted text-center"><?php echo $articles[0]['content']; ?> </p>
+                        <p class="text-muted text-center"><img src="../assets/img/oeuvre/<?php echo $oeuvres[0]['image']; ?>" alt="" width="70%"> </p>
                         <div class="d-flex justify-content-between">
-                            <small class="text-muted"><?php echo $articles[0]['date']; ?> </small>
+                            <small class="text-muted"><?php echo $oeuvres[0]['date']; ?> </small>
                             <small class="text-muted"><?php $user = $db->oneAutor($id_user); echo $user[0]['name']; ?></small>
 
 
